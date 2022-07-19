99b293263b77833b7f3a538a25c0084b664a406b
Chy <1552358695@qq.com>
2022-07-19 12:17:57 +0800

    初始化样式

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+// 测试请求
+import request from '@/utils/request.ts'
+const test = async () => {
+  const res = await request.get('/home/index')
+  console.log(res)
+}
+test ()
+</script>
 <template>
-<div>
-  app
-</div>
+app
+<p>123</p>
+<ul>
+  <li>
+    111
+  </li>
+</ul>
+<a href="#">链接🔗</a>
 </template>
-<style></style>
+<style lang="less" scoped>
+
+.box {
+  width: 300px;
+  height: 300px;
+   background-color: @warnColor;
+  .hoverShadow();
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+// const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  // less变量和mixins样式统一注入
+  css: {
+    preprocessorOptions: {
+      less: {
+        additionalData: `
+          @import "@/assets/styles/variables.less";
+          @import "@/assets/styles/mixins.less";
+        `
+      }
+    }
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
